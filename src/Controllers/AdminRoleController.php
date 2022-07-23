16681d2f90ce0eacd1fc4039547df6ad60f8976b
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2022-07-23 18:35:49 +0300

    permission development

--- src/Controllers/AdminRoleController.php
+++ src/Controllers/AdminRoleController.php
@@ -14,65 +14,80 @@ use Tir\Crud\Controllers\CrudController;
 
 class AdminRoleController extends CrudController
 {
-    public function store(Request $request): JsonResponse
-    {
-
-        DB::transaction(function () use ($request) { // Start the transaction
-            $data = $request->all();
-            $userId = Auth::id();
-            $preparedData = [];
-            $role = Role::create(['name' => $data['name'], 'slug' => $data['slug']]);
-            foreach ($data['permissions'] as $module => $permission) {
-                foreach ($permission as $action => $access) {
-                    array_push($preparedData, [
-                            'role_id' => $role->id,
-                            'module'  => $module,
-                            'action'  => $action,
-                            'access'  => $access
-                        ]
-
-                    );
-                }
-            }
-            Permission::insert($preparedData);
-        });
-
-        return $this->storeReturn($request);
-
-    }
-
-    public function updateCrud(Request $request, $id, $item)
-    {
-        return DB::transaction(function () use ($request, $item) { // Start the transaction
-            $data = $request->all();
-            $userId = Auth::id();
-            $preparedData = [];
-            $item->update(['name' => $data['name'], 'slug' => $data['slug']]);
-
-            $item->permissions()->delete();
-
-
-            foreach ($data['permissions'] as $module => $permission) {
-                foreach ($permission as $action => $access) {
-                    array_push($preparedData, [
-                            'role_id' => $item->id,
-                            'module'  => $module,
-                            'action'  => $action,
-                            'access'  => $access
-                        ]
-
-                    );
-                }
-            }
-            Permission::insert($preparedData);
-            return $item;
-        });
-    }
 
     protected function setModel(): string
     {
         return Role::class;
     }
+
+
+    public function store(Request $request): JsonResponse
+    {
+        $role = DB::transaction(function () use ($request) { // Start the transaction
+                $data = $request->all();
+                $preparedData = [];
+                $role = Role::create(['name' => $data['name'], 'slug' => $data['slug']]);
+                foreach ($data['permissions'] as $module => $permission) {
+                    foreach ($permission as $action => $access) {
+                        array_push($preparedData, [
+                                'role_id' => $role->id,
+                                'module'  => $module,
+                                'action'  => $action,
+                                'access'  => $access
+                            ]
+
+                        );
+                    }
+                }
+                Permission::insert($preparedData);
+
+            return $role;
+        });
+
+        return $this->storeResponse($role);
+
+    }
+
+
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $item = $this->model->accessLevel()->findOrFail($id);
+        $item->scaffold();
+
+        $item = DB::transaction(function () use ($request, $item) { // Start the transaction
+                $data = $request->all();
+                $userId = Auth::id();
+                $preparedData = [];
+                $item->update(['name' => $data['name'], 'slug' => $data['slug']]);
+
+                $item->permissions()->delete();
+
+
+                foreach ($data['permissions'] as $module => $permission) {
+                    foreach ($permission as $action => $access) {
+                        array_push($preparedData, [
+                                'role_id' => $item->id,
+                                'module'  => $module,
+                                'action'  => $action,
+                                'access'  => $access
+                            ]
+
+                        );
+                    }
+                }
+                Permission::insert($preparedData);
+                return $item;
+        });
+
+        $moduleName = $this->model->getModuleName();
+        $message = trans('core::message.item-updated', ['item' => trans("message.item.$moduleName")]); //translate message
+
+        return $this->updateResponse($item);
+
+    }
+
+
+
 
 
 }
---
+++ src/Requests/Permission.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tir\Authorization\Requests;
+
+
+use Tir\Crud\Support\Scaffold\Fields\Select;
+
+class Permission extends Select {
+
+    protected string $type = 'radio';
+
+    protected function setValue($model)
+    {
+        $filedName = explode(',', $this->name);
+
+        foreach($model->{$filedName[0]} as $permission){
+            if($permission['module'] == $filedName[1] && $permission['action'] == $filedName[2]){
+                $this->value = $permission['access'];
+            }
+        }
+    }
+}
