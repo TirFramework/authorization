c9f007617f4384556196d5644391f48c37db5e64
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2023-08-21 16:09:20 +0200

    Update admin.php

--- src/Routes/admin.php
+++ src/Routes/admin.php
@@ -3,12 +3,10 @@ use Illuminate\Support\Facades\Route;
 use Tir\Authorization\Controllers\AdminRoleController;
 
 // Add api middleware for use Laravel feature
-Route::group(['middleware' => 'auth:api', 'prefix' => 'api/v1'], function () {
+Route::group(['middleware' => config('crud.middlewares'), 'prefix' => 'api/v1/admin'], function () {
 
-    //Add admin prefix and middleware for admin area to user module
-    Route::group(['prefix' => 'admin', 'middleware' => 'IsAdmin'], function () {
         Route::resource('/role', AdminRoleController::class, ['names' => 'admin.role']);
-    });
 
 });
+
 
